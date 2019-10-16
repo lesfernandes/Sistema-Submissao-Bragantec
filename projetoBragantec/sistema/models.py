6c7ed9228d186778cbe1cf0713ce52e19478a9e5
lesoaress <lemayara16@gmail.com>
2019-10-16 12:32:33 -0300

    criação crud projeto

--- projetoBragantec/sistema/models.py
+++ projetoBragantec/sistema/models.py
@@ -3,6 +3,8 @@ from django.contrib.auth.models import AbstractUser
 
 # Create your models here.
 class Autor(models.Model):
+	escolhas = [(0,'Orientador'), (1,'Estudante')]
+	tipo = models.CharField(choices=escolhas, max_length=10, default=0)
 	email = models.EmailField('E-mail', unique=True, default=None)
 	nome = models.CharField('Nome', max_length=100, default=None)
 	idade = models.IntegerField('Idade', default=None)
@@ -15,4 +17,22 @@ class Autor(models.Model):
 
 	class Meta:
 		db_table = "autor"
+		verbose_name = "Autor"
+		verbose_name_plural = "Autores"
 			
+class Projeto(models.Model):
+	resumo = models.CharField(max_length=200)
+	email = models.ForeignKey(Autor, on_delete=models.CASCADE)
+	areas = [(0, 'Ciências Humanas e Linguagens'), 
+			(1, 'Ciências da Natureza e Exatas'), 
+			(2, 'Informática'),
+			(3, 'Engenharias')]
+	area = models.CharField(choices=areas, max_length=32)
+	palavras_chaves = models.CharField(max_length=100)
+	plano_de_pesquisa = models.FileField(upload_to='uploads/')
+	link_video = models.CharField(max_length=150)
+
+	class Meta:
+		verbose_name = "Projeto"
+		verbose_name_plural = "Projetos"
+		db_table = "projeto"
